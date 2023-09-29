50edbd922a4d2e329716d88e667e0aeebbe079c6
Daniel Murga <daniel.murga@suse.com>
2023-09-29 12:31:47 +0200

    1st commit

---
+++ neu_vector/all_in_one_docker.sh
@@ -0,0 +1,31 @@
+    
+    
+    #management allinone
+     docker run -d \
+       --name allinone \
+       --pid=host \
+       --privileged \
+       -e CLUSTER_JOIN_ADDR=192.168.1.117 \
+       -e NV_PLATFORM_INFO=platform=Docker \
+       -p 18300:18300 \
+       -p 18301:18301 \
+       -p 18400:18400 \
+       -p 18401:18401 \
+       -p 18301:18301/udp \
+       -p 8443:8443 \
+       -v /lib/modules:/lib/modules:ro \
+       -v /var/neuvector:/var/neuvector \
+       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       -v /sys/fs/cgroup:/host/cgroup:ro \
+       -v /proc:/host/proc:ro \
+       neuvector/allinone:latest    
+
+    #scanner image
+ 
+     docker run -td \
+       --name scanner \
+       -e CLUSTER_JOIN_ADDR=192.168.1.117 \
+       -e NV_PLATFORM_INFO=platform=Docker \
+       -p 18402:18402 \
+       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       neuvector/scanner:latest
